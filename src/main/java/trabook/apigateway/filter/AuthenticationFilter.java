33a684245dd856b4595eb9b0e9296887cc8c4101
Suyeong00 <hsy747@g.skku.edu>
2024-10-01 15:51:23 +0900

    change IP address

--- src/main/java/trabook/apigateway/filter/AuthenticationFilter.java
+++ src/main/java/trabook/apigateway/filter/AuthenticationFilter.java
@@ -107,7 +107,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     }
 
     private Mono<Void> renewTokenAndContinue(ServerWebExchange exchange, GatewayFilterChain chain, Integer userId, Config config) {
-        WebClient webClient = WebClient.builder().baseUrl("http://34.118.151.117:4060").build(); // auth-service URL
+        WebClient webClient = WebClient.builder().baseUrl("http://35.216.95.239:4060").build(); // auth-service URL
         log.info("start renewTokenAndContinue");
         log.info("userId={}", userId);
         return webClient.get()
